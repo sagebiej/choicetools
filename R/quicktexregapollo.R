4e5504ba7345173e6a15b0bf257c7f977b50013a
Julian Sagebiel <julian.sagebiel@idiv.de>
2025-08-04 13:00:24 +0200

    small fix in quicktexregapollo for wtp

--- R/quicktexregapollo.R
+++ R/quicktexregapollo.R
@@ -50,11 +50,12 @@ quicktexregapollo <- function(model = model, wtpest = NULL, se = "rob") {
     estimated <- wtpest
     colnames(estimated) <- c("estimate", "rob_s_e", "robt", "p_1_sided_2")
   }
-
-  coefnames <- rownames(estimated)
-
-  texout <- texreg::createTexreg(
-    coef.names = coefnames, coef = estimated[["estimate"]], se = estimated[["se"]], pvalues = estimated$pv,
+  
+  
+  coefnames <- gsub(pattern = "_[a-z]$", "", rownames(estimated))
+  
+  texout <- createTexreg(
+    coef.names = coefnames, coef = estimated[["estimate"]], se = estimated[["rob_s_e"]], pvalues = estimated$p_1_sided_2
     gof.names = c("No Observations", "No Respondents", "Log Likelihood (Null)", "Log Likelihood (Converged)"),
     gof = c(model[["nObsTot"]], model[["nIndivs"]], model[["LL0"]][[1]], model[["LLout"]][[1]]),
     gof.decimal = c(FALSE, FALSE, TRUE, TRUE)
